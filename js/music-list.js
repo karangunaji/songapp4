b20865cdbc7b95e174e6c238808cfe6c94369fe4
karan rathod <92873841+karangunaji@users.noreply.github.com>
2023-03-26 23:41:55 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,58 @@
+
+let allMusic = [
+    {
+        name: "Dal jhur ro  ",
+        artist: "Banjara Songs",
+        img: "dal",
+        src: "dal"
+      },
+    {
+      name: "Hue Bechain ",
+      artist: "Arijit Singh",
+      img: "bechain",
+      src: "bechain"
+    },
+    {
+      name: "Deewana",
+      artist: "Audio Library",
+      img: "deewanan",
+      src: "Deewana"
+    },
+    {
+      name: "Sakhiyaan ",
+      artist: "Manindar Buttar",
+      img: "Sakhiyaan",
+      src: "Sakhiyaan"
+    },
+    {
+      name: "Naino ki jo bat",
+      artist: "Rahat Ali Khan",
+      img: "Naino",
+      src: "Naino"
+    },
+    {
+      name: "Bol na mahi",
+      artist: "Arijit Singh",
+      img: "bol",
+      src: "bol"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+        name: "Chora sidha sadha",
+        artist: "NCS Release",
+        img: "Chora",
+        src: "Chora"
+      },
+      {
+        name: "Phir bhi tumko",
+        artist: "Arijit Singh",
+        img: "Phirbhi",
+        src: "Phirbhi"
+      },
+   
+  ];
